a3626112ca0fd2b76a9bb95f5deb16a191b1ab49
lanasergeeva <marikakopina@gmail.com>
2021-11-11 15:14:42 +0300

    1. Интеграционные тесты с Hqldb [#4746 #227330] задание зи урока

---
+++ src/test/java/ru/java/integration/CityStoreTest.java
@@ -0,0 +1,28 @@
+package ru.java.integration;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CityStoreTest  {
+    @Test
+    public void whenCreate() {
+        CityStore cityStore = new CityStore();
+        City city = new City("Moscow");
+        cityStore.create(city);
+        List<City> all = cityStore.findAll();
+        assertEquals(city, all.get(0));
+    }
+
+    @Test
+    public void whenFindAll() {
+        CityStore cityStore = new CityStore();
+        City city1 = new City("Moscow");
+        City city2 = new City("St. Petersburg");
+        cityStore.create(city1);
+        cityStore.create(city2);
+        assertEquals(List.of(city1, city2), cityStore.findAll());
+    }
+}
