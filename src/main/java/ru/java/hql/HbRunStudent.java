f50f216c80f58069c99e32b8abc40f5d76669214
lanasergeeva <marikakopina@gmail.com>
2021-11-11 15:16:21 +0300

    0. HQL [#6874]

---
+++ src/main/java/ru/java/hql/HbRunStudent.java
@@ -0,0 +1,52 @@
+package ru.java.hql;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
+
+public class HbRunStudent {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+/*
+            Student one = Student.of("Alex", 21, "Moscow");
+            Student two = Student.of("Nikolay", 28, "Saint-Petersburg");
+            Student three = Student.of("Nikita", 25, "Kaliningrad");
+
+            session.save(one);
+            session.save(two);
+            session.save(three);*/
+/*
+            Query query = session.createQuery("from Student");
+            for (Object st : query.list()) {
+                System.out.println(st);
+            }
+
+            Query query2 = session.createQuery(
+                    "update Student s set s.age = :newAge, s.city = :newCity where s.id = :fId"
+            );
+            query2.setParameter("newAge", 22);
+            query2.setParameter("newCity", "London");
+            query2.setParameter("fId", 1);
+            query2.executeUpdate();
+
+            Query query3 = session.createQuery("from Student s where s.id = 1");
+            System.out.println(query3.uniqueResult());*/
+
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
---
+++ src/main/java/ru/java/hql/HbmRunCandidate.java
@@ -0,0 +1,60 @@
+package ru.java.hql;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
+
+public class HbmRunCandidate {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            Candidate one = Candidate.of("Ivan", 2, 2800.58);
+            Candidate two = Candidate.of("Oleg", 5, 3000.99);
+            Candidate three = Candidate.of("Valeriy", 1, 800.60);
+            session.save(one);
+            session.save(two);
+            session.save(three);
+
+            Query query = session.createQuery("from Candidate");
+            for (Object st : query.list()) {
+                System.out.println(st);
+            }
+
+            Query query2 = session.createQuery(
+                    "update Candidate c set c.name = :newName, "
+                            + "c.experience = :newExp, c.salary =:newSalary where c.id = :fId"
+            );
+            query2.setParameter("newName", "Victor");
+            query2.setParameter("newExp", 3);
+            query2.setParameter("newSalary", 1250.77);
+            query2.setParameter("fId", 3);
+            query2.executeUpdate();
+
+            Query query3 = session.createQuery("from Candidate c where c.id = 1");
+            System.out.println(query3.uniqueResult());
+
+            Query query4 = session.createQuery("from Candidate c where c.name = :fName");
+            query4.setParameter("fName", "Victor");
+            System.out.println(query4.uniqueResult());
+
+            session.createQuery("delete from Candidate where id = :fId")
+                    .setParameter("fId", 2);
+
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
